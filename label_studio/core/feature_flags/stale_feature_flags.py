728f903fe7be58d3fc2b471353a663013686f52b
Raul Martin <luarmr@gmail.com>
2025-02-04 08:02:17 -0800

    refactor: OPTIC-1621: Remove Stale Feature Flag - fflag_perf_back_lsd… (#7014)

--- label_studio/core/feature_flags/stale_feature_flags.py
+++ label_studio/core/feature_flags/stale_feature_flags.py
@@ -3,7 +3,6 @@ STALE_FEATURE_FLAGS = {
     'fflag_feat_front_optic_66_lazy_chart_evaluation_19092023_short': False,
     'fflag_fix_back_leap_24_tasks_api_optimization_05092023_short': False,
     'fflag_feat_optic_2_ensure_draft_saved_short': True,
-    'fflag_perf_back_lsdv_4695_update_prediction_query_to_use_direct_project_relation': True,
     'fflag_feat_front_lsdv_4620_outliner_optimization_310723_short': True,
     'fflag_fix_front_lsdv_4600_lead_time_27072023_short': False,
     'fflag_feat_front_lops_e_10_updated_ux_short': False,
--- label_studio/projects/models.py
+++ label_studio/projects/models.py
@@ -5,7 +5,6 @@ import logging
 from typing import Any, Mapping, Optional
 
 from annoying.fields import AutoOneToOneField
-from core.feature_flags import flag_set
 from core.label_config import (
     check_control_in_config_by_regex,
     check_toname_in_config_by_regex,
@@ -327,13 +326,7 @@ class Project(ProjectMixin, models.Model):
 
     @property
     def has_any_predictions(self):
-        if flag_set(
-            'fflag_perf_back_lsdv_4695_update_prediction_query_to_use_direct_project_relation',
-            user='auto',
-        ):
-            return Prediction.objects.filter(Q(project=self.id)).exists()
-        else:
-            return Prediction.objects.filter(Q(task__project=self.id)).exists()
+        return Prediction.objects.filter(Q(project=self.id)).exists()
 
     @property
     def business(self):
@@ -933,14 +926,7 @@ class Project(ProjectMixin, models.Model):
         :param extended: Boolean, if True, returns additional information. Default is False.
         :return: Dict or list containing model versions and their count predictions.
         """
-        if flag_set(
-            'fflag_perf_back_lsdv_4695_update_prediction_query_to_use_direct_project_relation',
-            user='auto',
-        ):
-            predictions = Prediction.objects.filter(project=self)
-        else:
-            predictions = Prediction.objects.filter(task__project=self)
-        # model_versions = set(predictions.values_list('model_version', flat=True).distinct())
+        predictions = Prediction.objects.filter(project=self)
 
         if extended:
             model_versions = list(
